78637413d0a3ad8fe6b376cd8e1a1129381cc5ef
jurez <@>
2015-02-16 21:50:10 +0100

    rewire home controller, expose elasticsearch outside vagrant

--- src/RadioStudent/AppBundle/Controller/DefaultController.php
+++ src/RadioStudent/AppBundle/Controller/DefaultController.php
@@ -9,16 +9,13 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/")
      * @Template()
      */
-    public function indexAction($name)
+    public function indexAction()
     {
         return $this->render(
-            '::index.html.twig',
-            [
-                'data' => $name
-            ]
+            '::index.html.twig', []
         );
     }
 }
