57a3dc83f8a8569de873eafeba504440409774a8
kokoon <kokoon@users.noreply.github.com>
2015-02-21 17:22:56 +0100

    elastica stuff

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new RadioStudent\AppBundle\RadioStudentAppBundle(),
+            new FOS\ElasticaBundle\FOSElasticaBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
