415ac2856165cf9a6ef0ab89566947514f00714e
mzgajner <mzgajner@astina.ch>
2015-03-01 19:14:18 +0100

    Add basic autocomplete implementation.

--- app/Resources/js/app.js
+++ app/Resources/js/app.js
@@ -4,19 +4,30 @@
 
 var picapicaApp = angular.module('picapicaApp', [
     'ngRoute',
+    'mgcrea.ngStrap',
     'picapicaControllers',
     'picapicaServices'
 ]);
 
-picapicaApp.config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider.
-            when('/search', {
-                templateUrl: 'partials/track-search.html',
-                controller: 'TrackSearchCtrl'
-            }).
-            otherwise({
-                redirectTo: '/search'
-            });
-    }
-]);
+picapicaApp
+    .config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.
+                when('/search', {
+                    templateUrl: 'partials/track-search.tpl.html',
+                    controller: 'TrackSearchCtrl'
+                }).
+                otherwise({
+                    redirectTo: '/search'
+                });
+        }
+    ])
+    .config(function($typeaheadProvider) {
+        angular.extend($typeaheadProvider.defaults, {
+            minLength: 3,
+            delay: 250,
+            trigger: 'click',
+            template: 'partials/_autocomplete.tpl.html',
+            limit: 30
+        });
+    });
--- app/Resources/js/controllers.js
+++ app/Resources/js/controllers.js
@@ -4,7 +4,8 @@
 
 var picapicaControllers = angular.module('picapicaControllers', []);
 
-picapicaControllers.controller('TrackSearchCtrl', ['Track', '$scope', function(Track, $scope) {
+picapicaControllers.controller('TrackSearchCtrl', ['Track', '$scope', '$http', '$q', function(Track, $scope, $http, $q) {
+    var canceler = $q.defer();
     $scope.tracks = [];
     $scope.searchTerm = '';
 
@@ -17,4 +18,32 @@ picapicaControllers.controller('TrackSearchCtrl', ['Track', '$scope', function(T
         );
         $scope.searchTerm = '';
     };
+
+    $scope.getSuggestions = function(searchInput) {
+        canceler.resolve(true);
+        canceler = $q.defer();
+        return $http({
+            method: 'GET',
+            url: 'api/v1/ac',
+            params: { search: searchInput },
+            timeout: canceler.promise
+        }).then(function(res) {
+            var results = res.data;
+
+            for (var key in results) {
+                if(results[key].length > 0) {
+                    results[key].forEach(function(result){
+                        result.type = key;
+                    });
+                }
+            }
+
+            return results.artists.concat(results.albums, results.tracks);
+        });
+    };
+
+    $scope.$on('$typeahead.select', function(value, index){
+        $scope.searchTerm = '';
+    });
+
 }]);
