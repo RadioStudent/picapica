0ba87131884486f7528e2d4eae5f2007683138f9
kokoon <kokoon@users.noreply.github.com>
2015-03-22 11:27:58 +0100

    update symfony, setup VarDumper

--- app/autoload.php
+++ app/autoload.php
@@ -1,7 +1,17 @@
-<?php
+    <?php
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
+use Symfony\Component\VarDumper\VarDumper;
+
+if (!function_exists('dump')) {
+    function dump($var)
+    {
+        foreach (func_get_args() as $var) {
+            VarDumper::dump($var);
+        }
+    }
+}
 
 /**
  * @var ClassLoader $loader
