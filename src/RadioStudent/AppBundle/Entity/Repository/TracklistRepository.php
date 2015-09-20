de13e3e36c05be3fdb04a5299ed1de0234edad39
kokoon <kokoon@users.noreply.github.com>
2015-09-20 15:44:47 +0200

    fix phpdoc

--- src/RadioStudent/AppBundle/Entity/Repository/TracklistRepository.php
+++ src/RadioStudent/AppBundle/Entity/Repository/TracklistRepository.php
@@ -14,6 +14,8 @@ class TracklistRepository extends EntityRepository
     /**
      * @param Author       $author
      * @param ParameterBag $tracklistData
+     *
+     * @return Tracklist
      */
     public function create($author, $tracklistData)
     {
