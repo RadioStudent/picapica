d9f80d4388b1732537b279a18b632d66db9779d5
Jurij Podgoršek <podgorsek.jurij@gmail.com>
2019-08-13 18:20:46 +0200

    WIP ldap login, version bump in composer

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new FR3D\LdapBundle\FR3DLdapBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
